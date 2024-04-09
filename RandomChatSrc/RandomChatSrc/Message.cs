08e1579f48a55633bd4c07f599ec3fc2914b67b9
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-09 16:25:14 +0300

    Create Message.cs
    Created the class.

---
+++ RandomChatSrc/RandomChatSrc/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace domain
+{
+	public class Message(int id, String textChatFolderPath, String messagePath, DateTime sentTime, String content)
+    {
+        private int Id { get; } = id;
+        private string TextChatFolderPath { get; } = textChatFolderPath;
+		private string MessagePath { get; } = messagePath;
+		private DateTime SentTime { get; } = sentTime;
+		private string Content { get; } = content;
+    }
+}
