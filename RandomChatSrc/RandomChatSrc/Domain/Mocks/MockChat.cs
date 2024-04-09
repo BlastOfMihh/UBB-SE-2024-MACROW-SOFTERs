8f01bf3b4914047cbbd03db9b9ce6dde71493c11
Richard Toth <richard.toth@kadevee.com>
2024-04-09 18:41:53 +0300

    added functionalities fo ChatroomsManagerService

---
+++ RandomChatSrc/RandomChatSrc/Domain/Mocks/MockChat.cs
@@ -0,0 +1,17 @@
+﻿using AudioUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChatSrc.Domain.Mocks
+{
+    public class MockChat : IMockChat
+    {
+        public Guid Id { get; set; }
+        private List<string> Participants { get; set; }
+        private int MaxParticipants { get; set; }
+
+    }
+}
---
+++ RandomChatSrc/RandomChatSrc/Services/ChatroomsManagementService/IChatroomsManagementService.cs
@@ -0,0 +1,17 @@
+﻿using RandomChatSrc.Domain.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChatSrc.Services.ChatroomsManagementService
+{
+    public interface IChatroomsManagementService
+    {
+        MockChat GetChat();
+        MockChat CreateChat(int size);
+        void DeleteChat(Guid id);
+
+    }
+}
