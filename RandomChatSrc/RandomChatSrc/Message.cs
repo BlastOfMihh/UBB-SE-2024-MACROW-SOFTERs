e9b1b3c879c72dcf95ccb8a86e44310075fb687c
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-09 20:19:02 +0300

    Update Message.cs
    modified some access modifiers bcz + made `id` field of `Guid` type.

--- RandomChatSrc/RandomChatSrc/Message.cs
+++ RandomChatSrc/RandomChatSrc/Message.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 
 
 namespace domain
 {
-    public class Message(int id, int senderId, String textChatFolderPath, String messagePath, DateTime sentTime, String content)
+    public class Message(Guid id, int senderId, String textChatFolderPath, String messagePath, DateTime sentTime, String content)
     {
-        private int Id { get; } = id;
-        private string TextChatFolderPath { get; } = textChatFolderPath;
-	private string MessagePath { get; } = messagePath;
-	private DateTime SentTime { get; } = sentTime;
-	private string Content { get; } = content;
-        private string SenderId { get; } = senderId;
+        private Guid Id { get; } = id;
+        public string TextChatFolderPath { get; } = textChatFolderPath;
+        public string MessagePath { get; } = messagePath;
+        public DateTime SentTime { get; } = sentTime;
+        public string Content { get; } = content;
+        public int SenderId { get; } = senderId;
     }
 }
