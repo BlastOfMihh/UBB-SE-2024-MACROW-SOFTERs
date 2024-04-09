fe91c283799145e3fdbc3399886e16b0b90a2e9c
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-09 18:06:26 +0300

    add SenderId field

--- RandomChatSrc/RandomChatSrc/Message.cs
+++ RandomChatSrc/RandomChatSrc/Message.cs
@@ -3,12 +3,13 @@
 
 namespace domain
 {
-    public class Message(int id, String textChatFolderPath, String messagePath, DateTime sentTime, String content)
+    public class Message(int id, int senderId, String textChatFolderPath, String messagePath, DateTime sentTime, String content)
     {
         private int Id { get; } = id;
         private string TextChatFolderPath { get; } = textChatFolderPath;
 	private string MessagePath { get; } = messagePath;
 	private DateTime SentTime { get; } = sentTime;
 	private string Content { get; } = content;
+        private string SenderId { get; } = senderId;
     }
 }
