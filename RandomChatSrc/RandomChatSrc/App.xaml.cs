a7cc20d481ecfbf2dbef22c86533ed6591dab11e
tibumanu <tibu.cristian.sdf@gmail.com>
2024-04-09 22:48:53 +0300

    summary required
    TextChat.cs not added

--- RandomChatSrc/RandomChatSrc/App.xaml.cs
+++ RandomChatSrc/RandomChatSrc/App.xaml.cs
@@ -6,7 +6,16 @@
         {
             InitializeComponent();
 
+<<<<<<< Updated upstream
             MainPage = new AppShell();
+=======
+            MainPage = new NavigationPage(new OpenChatsWindow());
+
+            // In order to see the chatroom page uncomment the below line and comment the other main page
+            // MainPage = new NavigationPage(new ChatRoomPage());
+
+            //MainPage = new AppShell();
+>>>>>>> Stashed changes
         }
     }
 }
