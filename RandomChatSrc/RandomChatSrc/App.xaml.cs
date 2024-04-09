8090989bcd1ab21ee2b6b31060ce88a266bbb1d7
tibumanu <tibu.cristian.sdf@gmail.com>
2024-04-09 23:07:22 +0300

    mock clicking on chats opens a chat
    idk

--- RandomChatSrc/RandomChatSrc/App.xaml.cs
+++ RandomChatSrc/RandomChatSrc/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace RandomChatSrc
+﻿using RandomChatSrc.Pages;
+
+namespace RandomChatSrc
 {
     public partial class App : Application
     {
@@ -6,16 +8,13 @@
         {
             InitializeComponent();
 
-<<<<<<< Updated upstream
-            MainPage = new AppShell();
-=======
+
             MainPage = new NavigationPage(new OpenChatsWindow());
 
             // In order to see the chatroom page uncomment the below line and comment the other main page
             // MainPage = new NavigationPage(new ChatRoomPage());
 
             //MainPage = new AppShell();
->>>>>>> Stashed changes
         }
     }
 }
