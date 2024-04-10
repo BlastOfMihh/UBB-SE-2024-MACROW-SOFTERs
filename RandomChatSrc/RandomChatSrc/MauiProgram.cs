4462ac15b2e82e74564c5a475bd66d96ae5cb9c3
Richard Toth <richardtoth603@gmail.com>
2024-04-11 02:51:32 +0300

    god finally it works

--- RandomChatSrc/RandomChatSrc/MauiProgram.cs
+++ RandomChatSrc/RandomChatSrc/MauiProgram.cs
@@ -3,18 +3,33 @@ using RandomChatSrc.Services.ChatroomsManagement;
 using RandomChatSrc.Pages;
 using RandomChatSrc.Services.RandomMatchingService;
 using System.Diagnostics;
+using RandomChatSrc.Domain.TextChat;
 
 namespace RandomChatSrc
 {
     public static class MauiProgram
         // va bat
     {
+        public static void test()
+        {
+            //test the ChatroomsManagementService
+            ChatroomsManagementService chatroomsManagementService = new ChatroomsManagementService();
+            chatroomsManagementService.CreateChat(5);
+            var allChats = chatroomsManagementService.getAllChats();
+            foreach (TextChat chat in allChats)
+            {
+                chat.AddMessage("asd-asd-asd-asd", "Hello");
+                chat.AddMessage("asd-dfd-asd-asd", "Hello there");
+                Trace.WriteLine(chat.availableParticipantsCount());
+            }
+        }
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
             builder.Services.AddSingleton<IChatroomsManagementService, ChatroomsManagementService>();
             builder.Services.AddSingleton<IRandomMatchingService, RandomMatchingService>();
             Trace.WriteLine("Hello World");
+            test();
             //test git push
             builder
                 .UseMauiApp<App>()
