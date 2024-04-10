fb50c27b15cf054962fc470fb9da14852f50115c
Richard Toth <richardtoth603@gmail.com>
2024-04-10 16:09:37 +0300

    first commit for branch

---
+++ RandomChatSrc/RandomChatSrc/Services/UserChatListService/UserChatListService.cs
@@ -0,0 +1,17 @@
+﻿using RandomChatSrc.Domain.TextChat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChatSrc.Services.UserChatListService
+{
+    internal class UserChatListService : IUserChatListService
+    {
+        public List<TextChat> getOpenChats()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
