f8df8bf417d5f2d887c31521adc5b67f0d27426a
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-10 19:17:02 +0300

    changed SenderId from int to string.

--- RandomChatSrc/RandomChatSrc/Message.cs
+++ RandomChatSrc/RandomChatSrc/Message.cs
@@ -3,13 +3,13 @@ using System;
 
 namespace domain
 {
-    public class Message(Guid id, int senderId, String textChatFolderPath, String messagePath, DateTime sentTime, String content)
+    public class Message(Guid id, string senderId, String textChatFolderPath, String messagePath, DateTime sentTime, String content)
     {
         private Guid Id { get; } = id;
         public string TextChatFolderPath { get; } = textChatFolderPath;
         public string MessagePath { get; } = messagePath;
         public DateTime SentTime { get; } = sentTime;
         public string Content { get; } = content;
-        public int SenderId { get; } = senderId;
+        public string SenderId { get; } = senderId;
     }
 }
