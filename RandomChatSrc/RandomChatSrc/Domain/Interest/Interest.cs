36699783c4a356da9fe6f5d5ff808f8115e55236
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-13 22:41:17 +0300

    changes to interest class

--- RandomChatSrc/RandomChatSrc/Domain/Interest/Interest.cs
+++ RandomChatSrc/RandomChatSrc/Domain/Interest/Interest.cs
@@ -16,5 +16,18 @@ namespace RandomChatSrc.Domain.Interest
             this.Id = Guid.NewGuid();
             this.InterestName = interestName;
         }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            Interest other = (Interest)obj;
+            return this.Id == other.Id;  // or should compare `this.InterestName == other.InterestName`? probably not
+        }
     }
 }
