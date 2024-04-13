05abe79525648d63e5ec401a916fd9b47ed876f6
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-13 21:19:34 +0300

    initial commit, first version of Interest class.

---
+++ RandomChatSrc/RandomChatSrc/Domain/Interest/Interest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChatSrc.Domain.Interest
+{
+    public class Interest
+    {
+        public Guid Id { get; }
+        public string InterestName { get; }
+
+        public Interest(string interestName)
+        {
+            this.Id = Guid.NewGuid();
+            this.InterestName = interestName;
+        }
+    }
+}
