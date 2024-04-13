36a57f4b48ccb4926941f57be9c4cc5c87102cbf
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-13 22:38:49 +0300

    implemented Equals method

--- RandomChatSrc/RandomChatSrc/Domain/Interest/Interest.cs
+++ RandomChatSrc/RandomChatSrc/Domain/Interest/Interest.cs
@@ -16,5 +16,19 @@ namespace RandomChatSrc.Domain.InterestDomain
             this.Id = Guid.NewGuid();
             this.InterestName = interestName;
         }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            Interest other = (Interest)obj;
+            return this.Id == other.Id;  // or should compare `this.InterestName == other.InterestName`? probably not
+        }
     }
 }
