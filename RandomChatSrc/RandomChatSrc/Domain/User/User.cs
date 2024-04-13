ecdff6d9f6a850f71e0ad870e8b536d7a2ba07b6
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-13 21:35:52 +0300

    abc

--- RandomChatSrc/RandomChatSrc/Domain/User/User.cs
+++ RandomChatSrc/RandomChatSrc/Domain/User/User.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace RandomChatSrc.Domain.UserDomain
 {
     public class User
     {
         public Guid id { get; set; }
         public string name { get; set; }
+
+        public List<Interest> interests { get; set; }
 
         public User(string name)
         {
