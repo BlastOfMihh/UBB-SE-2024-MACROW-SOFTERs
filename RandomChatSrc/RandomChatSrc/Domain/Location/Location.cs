dd567f194d139b8b851f9b6ca1f4ea9b728ffbd0
Richard Toth <richardtoth603@gmail.com>
2024-04-17 13:16:01 +0300

    location domain added

---
+++ RandomChatSrc/RandomChatSrc/Domain/Location/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChatSrc.Domain.Location
+{
+    public class Location
+    {
+        public float xCoord { get; set; }
+        public float yCoord { get; set; }
+        public string description { get; set; }
+        public Location(float xCoord, float yCoord, string description="")
+        {
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+            this.description = description;
+        }
+    }
+}
