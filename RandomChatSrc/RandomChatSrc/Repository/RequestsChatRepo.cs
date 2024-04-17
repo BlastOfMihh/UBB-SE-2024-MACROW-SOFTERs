58386613d6a49a8b385e887b674cfcd6c609d83d
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-17 14:45:55 +0300

    have to do this just so i can look at another branch yay

---
+++ RandomChatSrc/RandomChatSrc/Repository/RequestsChatRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomChatSrc.Domain.RequestDomain;
+
+namespace RandomChatSrc.Repository
+{
+    public class RequestsChatRepo
+    {
+        public List<Request> Requests {  get; set; }
+        public string RequestsFolderPath {  get; set; }
+
+        public RequestsChatRepo(List<Request> requests, string requestsFolderPath)
+        {
+            this.Requests = requests;
+            this.RequestsFolderPath = requestsFolderPath;
+        }
+
+        private void loadFromMemory()
+        {
+
+        }
+    }
+}
