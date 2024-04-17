75d9e8d4c6479d028aee738bc87de1fd35727d56
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-17 14:34:04 +0300

    initial implementation of the class

---
+++ RandomChatSrc/RandomChatSrc/Domain/Request/Request.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChatSrc.Domain.RequestDomain
+{
+    public class Request
+    {
+        public Guid SenderUserId { get; set; }
+        public Guid ReceiverUserId { get; set; }
+        public string requestPath;
+
+        public Request(Guid senderUserId, Guid receiverUserId, string requestPath)
+        {
+            this.SenderUserId = senderUserId;
+            this.ReceiverUserId = receiverUserId;
+            this.requestPath = requestPath;
+        }
+    }
+}
