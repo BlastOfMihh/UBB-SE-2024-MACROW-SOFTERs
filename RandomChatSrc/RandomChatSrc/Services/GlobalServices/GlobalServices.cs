ca5ba6a44eeee6653591e124a180675811d685d0
Mihnea Alexandru Turcu <42068395+BlastOfMihh@users.noreply.github.com>
2024-04-17 11:01:25 +0300

    modified:   RandomChatSrc/RandomChatSrc/Services/ChatroomsManagement/ChatroomsManagementService.cs 	new file:   RandomChatSrc/RandomChatSrc/Services/GlobalServices/GlobalServices.cs 	modified:   RandomChatSrc/RandomChatSrc/Services/RandomMatchingService/RandomMatchingService.cs

---
+++ RandomChatSrc/RandomChatSrc/Services/GlobalServices/GlobalServices.cs
@@ -0,0 +1,16 @@
+﻿using RandomChatSrc.Services.ChatroomsManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChatSrc.Services.GlobalServices
+{
+    public class GlobalServices
+    {   
+        ChatroomsManagementService chatroomsManagementService;
+        public GlobalServices() { 
+        }
+    }
+}
--- RandomChatSrc/RandomChatSrc/Services/RandomMatchingService/RandomMatchingService.cs
+++ RandomChatSrc/RandomChatSrc/Services/RandomMatchingService/RandomMatchingService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Intents;
 using RandomChatSrc.Domain.ChatDomain;
 using RandomChatSrc.Domain.TextChat;
 using RandomChatSrc.Domain.UserConfig;
