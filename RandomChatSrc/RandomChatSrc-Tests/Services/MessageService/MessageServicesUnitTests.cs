12460dbb97435d6835785db50e71708660ba580c
Mark Kristo <99960484+Amouon@users.noreply.github.com>
2024-05-01 14:44:19 +0300

    Add tests for MessageService
    Having to constantly change the string gave me clinical depression

---
+++ RandomChatSrc/RandomChatSrc-Tests/Services/MessageService/MessageServicesUnitTests.cs
@@ -0,0 +1,57 @@
+﻿using RandomChatSrc.Services.MessageService;
+using Moq;
+using RandomChatSrc.Models;
+
+namespace RandomChatSrc_Tests.Services.MessageService
+{
+    [TestClass]
+    public class MessageServiceUnitTests
+    {
+        private Mock<TextChat> mockTextChat = null!;
+        private Guid testUserId;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockTextChat = new Mock<TextChat>();
+            testUserId = Guid.NewGuid();
+        }
+
+        [TestMethod]
+        public void Constructor_ValidParameters_CreatesInstance()
+        {
+            // Arrange & Act
+            var messageService = new RandomChatSrc.Services.MessageService.MessageService(mockTextChat.Object, testUserId);
+
+            // Assert
+            Assert.IsNotNull(messageService);
+        }
+
+        [TestMethod]
+        public void SendMessage_ValidMessage_CallsAddMessage()
+        {
+            // Arrange
+            var messageService = new RandomChatSrc.Services.MessageService.MessageService(mockTextChat.Object, testUserId);
+            string message = "Random test message";
+
+            // Act
+            messageService.SendMessage(message);
+
+            // Assert
+            mockTextChat.Verify(chat => chat.AddMessage(testUserId.ToString(), message), Times.Once);
+        }
+
+        [TestMethod]
+        public void GetTextChat_ReturnsTextChatInstance()
+        {
+            // Arrange
+            var messageService = new RandomChatSrc.Services.MessageService.MessageService(mockTextChat.Object, testUserId);
+
+            // Act
+            var result = messageService.GetTextChat();
+
+            // Assert
+            Assert.AreEqual(mockTextChat.Object, result);
+        }
+    }
+}
